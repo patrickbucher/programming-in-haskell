8827725c8bee4c1e5a4f0c800004da9eac3e5d93
Patrick Bucher <patrick.bucher@mailbox.org>
2020-07-08 07:30:38 +0200

    Caesar cipher: support lower and upper case letters

--- examples/caesar.hs
+++ examples/caesar.hs
@@ -1,17 +1,22 @@
 import Data.Char
 
+-- first change: lower and upper case
 let2int :: Char -> Int
-let2int c = ord c - ord 'a'
--- TODO: isLower: 'a', isUpper: 'A'
+let2int c | isLower c = ord c - ord 'a' + 26
+          | isUpper c = ord c - ord 'A'
+          | otherwise = ord c
 
+-- second change: lower and upper case
 int2let :: Int -> Char
-int2let n = chr (ord 'a' + n)
--- TODO: check if chr n in 'a'..'z' or 'A'..'Z'
+int2let n | n >= 0 && n < 26 = chr (ord 'A' + n)
+          | n >= 26 && n < 52 = chr (ord 'a' + n - 26)
+          | otherwise = chr n
 
+-- third change: lower and upper case
 shift :: Int -> Char -> Char
-shift n c | isLower c = int2let ((let2int c + n) `mod` 26)
+shift n c | isLower c = int2let ((let2int c + n - 26) `mod` 26 + 26)
+          | isUpper c = int2let ((let2int c + n) `mod` 26)
           | otherwise = c
--- TODO: isLower c or isUpper c
 
 encode :: Int -> String -> String
 encode n xs = [shift n x | x <- xs]
@@ -19,15 +24,20 @@ encode n xs = [shift n x | x <- xs]
 percent :: Int -> Int -> Float
 percent n m = (fromIntegral n / fromIntegral m) * 100
 
+--  third change: lowers becomes alphas
+alphas :: String -> Int
+alphas xs = sum [1 | x <- xs, isLower x || isUpper x]
+
+-- fourth change: toLowerString function
+toLowerString :: [Char] -> [Char]
+toLowerString xs = [toLower x | x <- xs]
+
+-- fifth change: convert to lower case for counting
 freqs :: String -> [Float]
-freqs xs = [percent (count x xs) n | x <- ['a'..'z']] where n = lowers xs
--- TODO: convert xs to lower before counting
+freqs xs = [percent (count x (toLowerString xs)) n | x <- ['a'..'z']] where n = alphas xs
 
 count :: Char -> String -> Int
 count x xs = sum [1 | x' <- xs, x' == x]
-
-lowers :: String -> Int
-lowers xs = sum [1 | x <- xs, isLower x]
 
 chisqr :: [Float] -> [Float] -> Float
 chisqr os es = sum [((o-e)^2)/e | (o,e) <- zip os es]
@@ -45,10 +55,10 @@ table' = freqs (encode 8 "there are a lot of programming languages out there")
 positions :: Eq a => a -> [a] -> [Int]
 positions x xs = [i | (x',i) <- zip xs [0..], x == x']
 
+-- sixth change: convert xs to lower case for frequency calculation
 crack :: String -> String
 crack xs = encode (-factor) xs
            where
                factor = head (positions (minimum chitab) chitab)
                chitab = [chisqr (rotate n table') table | n <- [0..25]]
-               table' = freqs xs
--- TODO: convert table' to lower first
+               table' = freqs (toLowerString xs)
